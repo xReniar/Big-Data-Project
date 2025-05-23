c3c0f00277a420a3315b5ca93171a6168c3f9704
xReniar <ilaorainer@gmail.com>
2025-05-23 20:43:01 +0200

    Remove data_cleaning folder

--- data_cleaning/clean_data.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-CSV_PATH = "../data/used_cars_data.csv"
-
-spark = SparkSession.builder.appName("Cleaning").getOrCreate()
-df = spark.read.csv(CSV_PATH, header=True)
-cleaned_df = df.na.drop()
