57d4a4f3bc05f43dd40c7bb099355964063b63ae
xReniar <ilaorainer@gmail.com>
2025-05-24 23:09:31 +0200

    Update start.sh

--- spark-core/start.sh
+++ spark-core/start.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# stop hadoop if already running
+$HADOOP_HOME/sbin/stop-dfs.sh
+
+# clean tmp folder
+rm -rf /tmp/*
+$HADOOP_HOME/bin/hdfs namenode -format
+
+# start hadoop
+$HADOOP_HOME/sbin/start-dfs.sh
