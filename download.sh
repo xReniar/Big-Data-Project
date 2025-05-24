1153713dda001aed3f3fd69cd6f9e347342d884f
xReniar <ilaorainer@gmail.com>
2025-05-24 23:12:02 +0200

    Remove .zip file after unzipping

--- download.sh
+++ download.sh
@@ -2,5 +2,6 @@
 
 mkdir data
 curl -L -o data/us-used-cars-dataset.zip https://www.kaggle.com/api/v1/datasets/download/ananaymital/us-used-cars-dataset
-cd data ; unzip us-used-cars-dataset.zip
-rm -rf data/us-used-cars-dataset.zip
+cd data
+unzip us-used-cars-dataset.zip
+rm -rf us-used-cars-dataset.zip
