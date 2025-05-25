866a80d1eface245ad0f0934dde0200b47d14a97
xReniar <ilaorainer@gmail.com>
2025-05-25 19:17:22 +0200

    Add dataset download command

--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 
 export ROOT_DIR=$(pwd)
 
+# download data
+mkdir data
+curl -L -o data/us-used-cars-dataset.zip https://www.kaggle.com/api/v1/datasets/download/ananaymital/us-used-cars-dataset
+cd data
+unzip us-used-cars-dataset.zip
+rm -rf us-used-cars-dataset.zip
+
+# start hadoop
 $HADOOP_HOME/sbin/stop-dfs.sh
 rm -rf /tmp/*
 $HADOOP_HOME/bin/hdfs namenode -format
