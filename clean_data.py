79c83483289f43993b7cd7c53237c9668ca693c4
xReniar <ilaorainer@gmail.com>
2025-05-27 00:03:11 +0200

    Update clean_data with clean_value()

--- clean_data.py
+++ clean_data.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-from pyspark.sql.types import IntegerType, DoubleType
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import IntegerType, DoubleType, StringType
 import os
+import re
 
+def clean_value(val):
+    if val is None:
+        return val
+    val = re.sub(r'[^a-zA-Z0-9_.,\- ]', '', val)
+    val = val.replace(',', ' ')
+    return val
 
 ROOT_DIR = os.getenv("ROOT_DIR")
 spark = SparkSession.builder.appName("data-cleaning").getOrCreate()
+
+clean_udf = udf(clean_value, StringType())
 
 df = spark.read \
     .option("header", True) \
@@ -24,14 +33,19 @@ df = spark.read \
         "year"
     )
 
+string_columns = [field.name for field in df.schema.fields if isinstance(field.dataType, StringType)]
+for col_name in string_columns:
+    df = df.withColumn(col_name, clean_udf(col(col_name)))
+
 df = df.filter(
     col("price").rlike("^[0-9]+(\.[0-9]+)?$") &
     col("year").rlike("^[0-9]{4}$")
 ).withColumn("price", col("price").cast(DoubleType())) \
  .withColumn("year", col("year").cast(IntegerType()))
 
-
 df.coalesce(1).write \
     .option("header", True) \
     .mode("append") \
     .csv(f"file://{ROOT_DIR}/data")
+
+spark.stop()
