aa25b829e0bb6bff75933ee0dcdb4228a08209b0
xReniar <ilaorainer@gmail.com>
2025-05-29 15:16:02 +0200

    Add mapper.py for job-2

---
+++ map-reduce/job-2/mapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+def price_category(price: str):
+    price = float(price)
+
+    if price >= 50000:
+        return "alto"
+    elif price >= 20000 and price < 50000:
+        return "medio"
+    else:
+        return "basso"
+
+for line in sys.stdin:
+    line = line.strip()
+    city, daysonmarket, description, _, _, _, _, price, year = line.split(",")
+
+    try:
+        daysonmarket = int(daysonmarket)
+        price_tag = price_category(price)
+        description = ",".join(description.split())
+
+        print(f"{city}::{year}::{price_tag}\t1::{daysonmarket}::[{description}]")
+    except ValueError:
+        continue
