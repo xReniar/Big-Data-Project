11e49f7e04b46785e8b7d75447349db0ee8c3985
xReniar <ilaorainer@gmail.com>
2025-05-29 15:17:28 +0200

    Move run.sh to root directory of map reduce folder

--- map-reduce/run.sh
+++ map-reduce/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-hdfs dfs -rm -r /user/$USER/map-reduce/job-1
+hdfs dfs -rm -r -f /user/$USER/map-reduce/$1
 hdfs dfs -mkdir -p /user/$USER/map-reduce/
 
 hadoop jar $HADOOP_HOME/streaming/hadoop-streaming-3.4.1.jar \
-    -mapper mapper.py \
-    -reducer reducer.py \
+    -mapper $1/mapper.py \
+    -reducer $1/reducer.py \
     -input /user/$USER/data/data_cleaned.csv \
-    -output /user/$USER/map-reduce/job-1
+    -output /user/$USER/map-reduce/$1/
