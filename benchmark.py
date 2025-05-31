1677740da9dd63fd5840d2f1d704411ea34a2448
xReniar <ilaorainer@gmail.com>
2025-05-31 17:54:22 +0200

    Add execution time graph image generation

--- benchmark.py
+++ benchmark.py
@@ -6,29 +6,44 @@ import matplotlib.pyplot as plt
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("tool", type=str, choices=["map-reduce", "spark-core", "spark-sql"], help="Tool to benchmark")
-parser.add_argument("--fractions", type=str, help="Dataset fractions to use for benchmark")
+parser.add_argument("job", type=str, choices=["job-1", "job-2"], help="Job name")
+parser.add_argument("--fractions", type=str, help="Fractions of dataset to use")
 args = parser.parse_args()
 
-datasets = list(map(lambda x: f"data-{float(x) * 100}%", args.fractions.split()))
+tools = ["map-reduce", "spark-core", "spark-sql"]
+fractions = list(map(lambda x: f"data-{float(x) * 100}%", args.fractions.split())) + ["data_cleaned"]
+exec_times = []
 
-for dataset in datasets:
-    for i in range(2):
+for tool in tools:
+    for fraction in fractions:
         start = time.time()
         process = subprocess.run(
-            ["bash", "run.sh", f"job-{i + 1}", dataset],
-            cwd=args.tool,
+            ["bash", "run.sh", args.job, fraction],
+            cwd=tool,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
         end = time.time()
-        print(f"Finished job-{i + 1} for dataset \"{dataset}\"")
-        exection_time = end - start
+        
+        print(f"Finished {args.job} for dataset \"{fraction}\"")
+        exec_times.append(end - start)
 
-        output_path = os.path.join("logs", args.tool, f"job-{i+1}")
+        output_path = os.path.join("logs", tool, args.job)
         os.makedirs(output_path, exist_ok=True)
-        with open(os.path.join(output_path, f"stdout-{dataset}.txt"), "wb") as f:
+        with open(os.path.join(output_path, f"stdout-{fraction}.txt"), "wb") as f:
             f.write(process.stdout)
 
-        with open(os.path.join(output_path, f"stderr-{dataset}.txt"), "wb") as f:
+        '''
+        with open(os.path.join(output_path, f"stderr-{fraction}.txt"), "wb") as f:
             f.write(process.stderr)
+        '''
+
+plt.figure(figsize=(8, 5))
+plt.plot(fractions, exec_times, marker='o', linestyle='-', color='blue', label='Execution Time')
+plt.xlabel('Dataset Fraction (%)')
+plt.ylabel('Processing Time (seconds)')
+plt.title(f'{args.tool}: {args.job}')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.savefig(os.path.join("logs", args.tool, f'benchmark_{args.tool}_{args.job}.png'), dpi=300)
