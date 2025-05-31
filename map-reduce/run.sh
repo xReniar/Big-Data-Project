972825388668c63a18df1bb898402f18b9d0920e
xReniar <ilaorainer@gmail.com>
2025-05-31 15:54:35 +0200

    Pass the name of the dataset directly as argument

--- map-reduce/run.sh
+++ map-reduce/run.sh
@@ -6,5 +6,5 @@ hdfs dfs -mkdir -p /user/$USER/map-reduce/
 hadoop jar $HADOOP_HOME/streaming/hadoop-streaming-3.4.1.jar \
     -mapper $1/mapper.py \
     -reducer $1/reducer.py \
-    -input /user/$USER/data/data_cleaned.csv \
+    -input /user/$USER/data/$2.csv \
     -output /user/$USER/map-reduce/$1/
