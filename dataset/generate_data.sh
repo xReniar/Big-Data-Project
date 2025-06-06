2feb4f7501539020ad4c061a95dcfa0777f82927
xReniar <ilaorainer@gmail.com>
2025-06-06 21:05:05 +0200

    Check master type and add $SPARK_CMD

--- dataset/generate_data.sh
+++ dataset/generate_data.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if [ "$1" != "local[*]" ] && [ "$1" != "yarn" ]; then
+    echo "Error: Invalid master argument. Use 'local[*]' or 'yarn'."
+    exit 1
+fi
+
+if [ "$1" == "local[*]" ]; then
+    SPARK_CMD="$SPARK_HOME/bin/spark-submit"
+else
+    SPARK_CMD="spark-submit"
+fi
+
 if [ ! -f "data/data_cleaned.csv" ]; then
-    $SPARK_HOME/bin/spark-submit \
-        --master local[*] \
+    $SPARK_CMD \
+        --master $1 \
         clean_data.py
 
     cd data
@@ -15,7 +26,7 @@ hdfs dfs -mkdir -p /user/$USER/data
 hdfs dfs -put data/data_cleaned.csv /user/$USER/data
 #hdfs dfs -mv /user/$USER/data/part-*.csv data/data_cleaned.csv
 
-$SPARK_HOME/bin/spark-submit \
-    --master local[*] \
+$SPARK_CMD \
+    --master $1 \
     generate_portions.py \
     --fractions "0.01 0.2 0.5 0.7"
