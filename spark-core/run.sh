d0804ad5eb2003b1208f5ed1751bd188ff39976f
xReniar <ilaorainer@gmail.com>
2025-06-06 21:05:45 +0200

    Add cluster compatibility

--- spark-core/run.sh
+++ spark-core/run.sh
@@ -3,8 +3,14 @@
 # remove output folder if already exists
 hdfs dfs -rm -r -f /user/$USER/spark-core/$1
 
-$SPARK_HOME/bin/spark-submit \
-    --master local[*] \
+if [ "$3" == "local[*]" ]; then
+    SPARK_CMD="$SPARK_HOME/bin/spark-submit"
+else
+    SPARK_CMD="spark-submit"
+fi
+
+$SPARK_CMD \
+    --master $3 \
     $1.py \
     -input /user/$USER/data/$2.csv \
     -output /user/$USER/spark-core/$1
