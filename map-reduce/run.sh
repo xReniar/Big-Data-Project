9523344c2415f2cb27a090887795c1e43733188e
xReniar <ilaorainer@gmail.com>
2025-06-06 21:05:23 +0200

    Add compatibility with cluster

--- map-reduce/run.sh
+++ map-reduce/run.sh
@@ -3,10 +3,18 @@
 hdfs dfs -rm -r -f /user/$USER/map-reduce/$1
 hdfs dfs -mkdir -p /user/$USER/map-reduce/
 
-hadoop jar $HADOOP_HOME/streaming/hadoop-streaming-3.4.1.jar \
-    -mapper $1/mapper.py \
-    -reducer $1/reducer.py \
-    -input /user/$USER/data/$2.csv \
-    -output /user/$USER/map-reduce/$1/
+if [ "$3" == "yarn" ]; then
+    hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+        -mapper $1/mapper.py \
+        -reducer $1/reducer.py \
+        -input /user/$USER/data/$2.csv \
+        -output /user/$USER/map-reduce/$1/
+else
+    hadoop jar "$HADOOP_HOME/streaming/hadoop-streaming.jar" \
+        -mapper $1/mapper.py \
+        -reducer $1/reducer.py \
+        -input /user/$USER/data/$2.csv \
+        -output /user/$USER/map-reduce/$1/
+fi
 
 hdfs dfs -cat /user/$USER/map-reduce/$1/part-* | head -n 10
