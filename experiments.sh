792f828155b6bb837d56e88bde1166d26ea53f35
xReniar <ilaorainer@gmail.com>
2025-06-06 21:04:32 +0200

    Add master type as argument for benchmark.py

--- experiments.sh
+++ experiments.sh
@@ -1,2 +1,2 @@
-python3 benchmark.py job-1 --fraction "0.01 0.2 0.5 0.7"
-python3 benchmark.py job-2 --fraction "0.01 0.2 0.5 0.7"
+python3 benchmark.py job-1 local[*] --fraction "0.01 0.2 0.5 0.7"
+python3 benchmark.py job-2 local[*] --fraction "0.01 0.2 0.5 0.7"
