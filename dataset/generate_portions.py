1b8dc1e8fd15d246129e499cde5bf94c1a7b2631
xReniar <ilaorainer@gmail.com>
2025-06-07 09:33:58 +0200

    Fix save process

--- dataset/generate_portions.py
+++ dataset/generate_portions.py
@@ -29,6 +29,7 @@ for fraction in fractions:
     df = spark.read \
         .csv(f"/user/{USER}/data/data_cleaned.csv", schema=schema) \
         .sample(withReplacement=False, fraction=fraction)
+    df.coalesce(1).write.csv(f"data/data-{fraction * 100}%")
     os.system(f"hdfs dfs -mv data/data-{fraction * 100}%/part-*.csv data/data-{fraction * 100}%.csv")
     os.system(f"hdfs dfs -get data/data-{fraction * 100}%.csv data")
     os.system(f"hdfs dfs -rm -r -f data/data-{fraction * 100}%")
