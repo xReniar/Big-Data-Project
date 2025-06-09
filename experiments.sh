bf6de150c537d7bd2da213bb49bbb48a881796b3
xReniar <ilaorainer@gmail.com>
2025-06-09 19:56:46 +0200

    Update bash usage

--- experiments.sh
+++ experiments.sh
@@ -1,2 +1,9 @@
-python3 benchmark.py job-1 local[*] --fraction "0.01 0.2 0.5 0.7"
-python3 benchmark.py job-2 local[*] --fraction "0.01 0.2 0.5 0.7"
+#!/usr/bin
+
+if [ "$1" != "local[*]" ] && [ "$1" != "yarn" ]; then
+    echo "Error: Invalid master argument. Use 'local[*]' or 'yarn'."
+    exit 1
+fi
+
+python3 benchmark.py job-1 $1 --fraction "0.01 0.2 0.5 0.7"
+python3 benchmark.py job-2 $1 --fraction "0.01 0.2 0.5 0.7"
